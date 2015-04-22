1118233858244a7ff7a127db15de446ef246ca4c
Tariq <csetariq@gmail.com>
2015-04-22 11:22:34 +0530

    remove using StringTokenizer

--- CarRental.java
+++ CarRental.java
@@ -1,29 +1,48 @@
 public class CarRental {
     
-    private static String allRentalSummary = new String();
-    
-    private static CarRental mostSpentRental = null;
-    private static CarRental leastSpentRental = null;
-    private static double minCharge = Double.MAX_VALUE;
-    private static double maxCharge = Double.MIN_VALUE;
-    
+    // List of all constants to be used
     private static final int    BASE_PERIOD         = 5;
     private static final int    INTERMEDIATE_PERIOD = 10;
     private static final double BASE_FARE           = 25.0;
     private static final double INTERMEDIATE_FARE   = 22.5;
     private static final double LONG_FARE           = 21.0;
+    private static final char   NEW_LINE            = '\n';
     
-    private static final String lineSeparator = System.lineSeparator();
     
+    /**
+    *   Concatenated list of all rental details in the following format
+    *
+    *       John Smith      10      Yes     $225.00
+    *       Josh Bloch      5       Yes     $125.00
+    *
+    */
+    private static String allRentalSummary = new String();
+    
+    // Set of references and primitive variables to track most/least spent rental
+    private static CarRental mostSpentRental = null;
+    private static CarRental leastSpentRental = null;
+    private static double minCharge = Double.MAX_VALUE;
+    private static double maxCharge = Double.MIN_VALUE;
+    
+    // To track rentals that are taken for less than a week or more
+    private static String lessThanSeven = "";
+    private static String greaterThanSeven = "";
+    
+    // Attributes of this (CarRental) class
     private String  name;
     private int     days;
     private boolean offerApplicable;
     private double  charge;
     
-    public CarRental(String name, int days, boolean offerApplicable) {
-        this.name = name;
-        this.days = days;
-        this.offerApplicable = offerApplicable;
+    public CarRental(String tempName, int tempDays, boolean tempOfferApplicable) {
+        name = tempName;
+        days = tempDays;
+        offerApplicable = tempOfferApplicable;
+        
+        if (days < 7)
+            lessThanSeven += "*";
+        else
+            greaterThanSeven += "*";
     }
     
     public String getName() {
@@ -52,25 +71,47 @@ public class CarRental {
         
         // If offer is applicable charge is reduced by 10%
         if (offerApplicable)
-            charge -= (charge * 10) / 100;
+            charge -= charge * (10 / 100);
             
+        // Keep track of most/least spent rental on the fly
         if (charge > maxCharge) {
             maxCharge = charge;
-            mostSpentRental = this;
-        } 
+            CarRental.mostSpentRental = this;
+        }
         
         if (charge < minCharge) {
             minCharge = charge;
-            leastSpentRental = this;
+            CarRental.leastSpentRental = this;
         }
+        
     }
     
+    // Displays the current rental in prescribed format. Called after getting each input
     public void displayInfo() {
         System.out.printf("\n\tThe rental of car hire from %s is $%.2f%n", name, charge);
         System.out.println("--------------------------------------------------------------------");
     }
     
-    public void addToSummary(String oneRental) {
-        allRentalSummary += lineSeparator + oneRental;
+    // Concatenates one rental to the summary with a newline
+    public static void addToSummary(String oneRental) {
+        allRentalSummary += oneRental + NEW_LINE;
+    }
+    
+    // Displays summary in the prescribed format
+    public static void displaySummary() {
+        System.out.println("\n\n\t\tSummary of Car Rentals\n");
+        System.out.println("===============================================================");
+        System.out.printf("%-20s %-15s %-15s %10s%n", "Name", "Days", "SpecialOffer", "Charge");
+        System.out.println("---------------------------------------------------------------");
+        
+        System.out.println(allRentalSummary);
+        
+        System.out.println();
+        System.out.println("---------------------------------------------------------------\n");
+        System.out.printf("The customer spending most rental is %s $%.2f%n%n", mostSpentRental.getName(), mostSpentRental.getCharge());
+        System.out.printf("The customer spending least rental is %s $%.2f%n%n", leastSpentRental.getName(), leastSpentRental.getCharge());
+        
+        System.out.println("The rental days < 7   : " + lessThanSeven);
+        System.out.println("The rental days >= 7  : " + greaterThanSeven);
     }
 }
--- CarRentalTest.java
+++ CarRentalTest.java
@@ -1,54 +1,42 @@
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class CarRentalTest {
     
-    // To be replaced with the maximum digit of my Student ID, minimum value is 4
-    private static final int N = 7;
+    private static final int N = 8;
     
     private static final String DOLLAR = "$";
-    private static final String FIELD_SEP = "@";
-    private static final String RECORD_SEP = "#";
     
     public static void main(String[] args) {
-        /**
-        *   Concatenation of all rentals
-        *   Each rental is separated by RECORD_SEP
-        *   Each field is separated by FIELD_SEP
-        */
-        String currentRental = new String();
-        
-        // To track the maximum and minimum rental
-        double minCharge = Double.MAX_VALUE;
-        double maxCharge = Double.MIN_VALUE;
-        CarRental mostSpentRental = null;
-        CarRental leastSpentRental = null;
-        
+              
         Scanner in = new Scanner(System.in);
         
         System.out.println("\n\n\t\tWelcome to use CarRental calculator");
+        
         for (int i = 0; i < N; i++) {
-            String name = null;
-            int days = 0;
+            String  name            = null;
+            int     days            = 0;
             boolean offerApplicable = false;
-            double charge = 0.0;
+            double  charge          = 0.0;
+            
+            String  currentRental   = new String();
             
             // Input data
+            // Repeat infinitely until proper input is given
             while (true) {
                 System.out.printf("\n\nEnter customer name %d: ", i + 1);
                 name = in.nextLine();
                 
-                if (!name.isEmpty()) // Positive case
+                if (!name.isEmpty()) // Positive case - Name is not empty
                     break;
                 else
-                    System.out.println("\nI hope you got a name");
+                    System.out.println("\nName cannot be empty");
             }
             
             while (true) {
                 System.out.print("\nEnter the number of days: ");
                 days = Integer.parseInt(in.nextLine());
                 
-                if (isDaysValid(days)) // Positive case
+                if (isDaysValid(days)) // Positive case - days is between 1 and 365 inclusive
                     break;
                 else
                     System.out.println("\nInvalid days. Please enter anywhere in [1-365]");
@@ -58,54 +46,40 @@ public class CarRentalTest {
                 System.out.print("\nEnter yes or no to indicate a special offer: ");
                 String answer = in.nextLine();
                 
-                if (isValidYesOrNo(answer)) { // Positive case
-                    offerApplicable = yesOrNo(answer);
+                if (isValidYesOrNo(answer)) { // Positive case - given answer is either "yes" or "no" irrespective of the case
+                    offerApplicable = yesOrNo(answer); // Convert "yes" or "no" to boolean 
                     break;
                 }
                 else
                     System.out.println("\nInvalid answer. Please answer either Yes or No");
             }
             
-            // Create CarRental objects with the input data
+            // Create CarRental object with the input data
             CarRental rental = new CarRental(name, days, offerApplicable);
             
             rental.calculateRental(); // Mandatory call, otherwise charge wont be calculated
             rental.displayInfo();
             
             charge = rental.getCharge();
+            String formattedCharge = DOLLAR + String.format("%.2f", charge); // Format the charge as String, to have 2 decimals precision and have preceded by '$' symbol e.g. $225.50
             
-            // appending the current rental in defined format
-            allRentals += name;
-            allRentals += FIELD_SEP;
-            allRentals += days;
-            allRentals += FIELD_SEP;
-            allRentals += offerApplicable;
-            allRentals += FIELD_SEP;
-            allRentals += String.format("%.2f",charge);
-            allRentals += RECORD_SEP;
+            currentRental += String.format("%-20s ", name);
+            currentRental += String.format("%-15d ", days);
+            currentRental += String.format("%-10s ", boolToYesOrNo(offerApplicable));
+            currentRental += String.format("%10s",formattedCharge);
             
-            // Track the maximum and minimum charge rentals
-            if (rental.getCharge() > maxCharge) {
-                maxCharge = rental.getCharge();
-                mostSpentRental = rental;
-            } 
-            
-            if (rental.getCharge() < minCharge) {
-                minCharge = rental.getCharge();
-                leastSpentRental = rental;
-            }
-            
+            CarRental.addToSummary(currentRental);
         }
         
         // Call a convenient method that takes care of displaying the summary
-        displaySummary(allRentals.toString(), mostSpentRental, leastSpentRental);
+        CarRental.displaySummary();
     }
     
     private static boolean isDaysValid(int days) {
         return days >= 1 && days <= 365;
     }
     
-    // Checks if the answer is either Yes or No
+    // Validates if the answer is either Yes or No
     private static boolean isValidYesOrNo(String answer) {
         return answer.equalsIgnoreCase("Yes") || 
                 answer.equalsIgnoreCase("No");
@@ -116,43 +90,11 @@ public class CarRentalTest {
         return answer.equalsIgnoreCase("Yes");
     }
     
-    private static void displaySummary(String allRentals, 
-                                        CarRental mostSpentRental, 
-                                        CarRental leastSpentRental) {
-        String lessThanSeven = "";
-        String greaterThanSeven = "";
-        StringTokenizer recordSplitter = null;
-        StringTokenizer fieldSplitter = null;
-        
-        System.out.println("\n\n\t\tSummary of Car Rentals\n");
-        System.out.println("===============================================================");
-        System.out.printf("%-20s %-15s %-15s %10s%n", "Name", "Days", "SpecialOffer", "Charge");
-        System.out.println("---------------------------------------------------------------");
-        
-        recordSplitter = new StringTokenizer(allRentals, RECORD_SEP);
-        while (recordSplitter.hasMoreTokens()) {
-            String rental = recordSplitter.nextToken();
-            
-            fieldSplitter = new StringTokenizer(rental, FIELD_SEP);
-            
-            String name             = fieldSplitter.nextToken();
-            String days             = fieldSplitter.nextToken();
-            String offerApplicable  = Boolean.valueOf(fieldSplitter.nextToken()) ? "Yes" : "No";
-            String charge           = DOLLAR + fieldSplitter.nextToken();
-            
-            System.out.printf("%-20s %-15s %-15s %10s%n", name, days, offerApplicable, charge);
-            
-            if (Integer.parseInt(days) < 7)
-                lessThanSeven += "*";
-            else
-                greaterThanSeven += "*";
-        }
-        System.out.println();
-        System.out.println("---------------------------------------------------------------\n");
-        System.out.printf("The customer spending most rental is %s $%.2f%n%n", mostSpentRental.getName(), mostSpentRental.getCharge());
-        System.out.printf("The customer spending least rental is %s $%.2f%n%n", leastSpentRental.getName(), leastSpentRental.getCharge());
-        
-        System.out.println("The rental days < 7   : " + lessThanSeven);
-        System.out.println("The rental days >= 7  : " + greaterThanSeven);
-    }
+    // Converts boolean to Yes or No String
+    private static String boolToYesOrNo(boolean value) {
+        if (value)
+            return "Yes";
+        else
+            return "No";
+    }    
 }
